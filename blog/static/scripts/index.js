b57addd2dc9ab72a0171a58603d7e35daac5033b
jackloo2809 <jacklewis2809@gmail.com>
2020-06-19 15:45:50 +0100

    finished home page

--- blog/static/scripts/index.js
+++ blog/static/scripts/index.js
@@ -1,9 +1,9 @@
 const menuToggle = document.getElementById("menu-toggle");
 const links = document.querySelector('#links');
 
-menuToggle.addEventListener("click", myFunction);
+menuToggle.addEventListener("click", navMenu);
 
-function myFunction() {
+function navMenu() {
     console.log(links.className)
     if(links.className != 'show-menu'){
         links.className = 'show-menu';
@@ -11,3 +11,20 @@ function myFunction() {
         links.className = '';
     }
 }
+
+
+
+//------------------------- script for about fade ---------------- //
+
+var featureBounds = document.getElementById("feature").getBoundingClientRect();
+var info = document.getElementById("info");
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+    
+    if(scrolled >=featureBounds.height*0.3){
+        info.style.opacity = 1;
+    }else{
+        info.style.opacity = 0;
+    }
+});
