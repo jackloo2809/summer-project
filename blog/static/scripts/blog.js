2119da123c960c80917a7dd81a6fc12be08cd496
jackloo2809 <jacklewis2809@gmail.com>
2020-06-22 15:41:46 +0100

    added scroll select function

--- blog/static/scripts/blog.js
+++ blog/static/scripts/blog.js
@@ -4,6 +4,55 @@ var descriptions = document.querySelectorAll('.text');
 var postLinks = document.getElementsByName(".links");
 
 
+/*
+var minheight = 0;
+var counter =0;
+var maxheight = document.body.scrollHeight;
+var increment = maxheight/titles.length;
+var currentpost = 0;
+*/
+var currentpost = 0;
+descriptions[currentpost].style.height =  descriptions[currentpost].scrollHeight;
+var step = (document.body.scrollHeight- document.querySelector("#content img").height) / titles.length;
+var counter = 0;
+console.log(step);
+
+document.body.addEventListener("wheel", ()=>{
+    
+    counter+= event.deltaY;
+
+    if(counter>step){
+        counter=0;
+        if(currentpost!=titles.length-1){
+            descriptions[currentpost].style.height = null;
+            currentpost++;
+            descriptions[currentpost].style.height =  descriptions[currentpost].scrollHeight;  
+        }
+    }
+    
+    if(counter < (step*-1)){
+        counter=0;
+        if(currentpost!=0){
+            descriptions[currentpost].style.height = null;
+            currentpost--;
+            descriptions[currentpost].style.height =  descriptions[currentpost].scrollHeight; 
+        }
+    }
+    /*
+    if((counter + event.deltaY)>=minheight && (counter + event.deltaY)<=maxheight){
+        counter += event.deltaY; 
+        
+        currentpost = Math.floor(counter/increment);
+    }
+    descriptions[currentpost].style.height =  descriptions[currentpost].scrollHeight; 
+    */
+});
+
+
+
+
+
+descriptions[currentpost].style.height =  descriptions[currentpost].scrollHeight; 
 
 function showText(){
     var index=0;
@@ -18,12 +67,9 @@ function showText(){
 
     
     if(collapsed){
-
-        console.log("initial height: " + descriptions[index].clientHeight);
         descriptions[index].style.height = height; 
     }else{
         descriptions[index].style.height = null;
-        console.log("expanded height: " + descriptions[index].clientHeight);
     }
     
     
