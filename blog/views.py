9d1732010ad38a9dd34c056b8d4e420400791e5f
jackloo2809 <jacklewis2809@gmail.com>
2020-07-13 17:23:06 +0100

    removed import for blog filter

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,6 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
-from .filters import Blog_filter
-from django.core.paginator import Paginator
 
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')[:3]
